38fde8d7007fe14655de0a643c7b23fa9a4819c3
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-13 10:49:34 -0700

    add routing

---
+++ _notes/trouble_shooting.md
@@ -0,0 +1,4 @@
+## unresolved reference django_app in Pycharm
+    PyCharm > Preferences -> Project Structure 
+    Right click on "django_proj", select Source
+    
--- django_proj/django_proj/urls.py
+++ django_proj/django_proj/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from django import views
+from django_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
